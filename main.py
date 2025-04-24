153432949f59c0d93b7ce845fcbab140b05c3854
Sagar Neupane <sagarneupane@Etechaces.com>
2025-04-24 18:44:14 +0530

    project setup

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import json
 from dotenv import load_dotenv
+import ast
 # from utils.faiss import build_faiss_index, search_faiss_index
 from utils.gpt_prompt import build_system_prompt, build_user_prompt
 
@@ -14,7 +15,13 @@ open_ai_key = os.getenv("OPEN_AI_KEY")
 open_ai_api_endpoints = os.getenv("OPEN_AI_API_ENDPOINT")
 deployment = os.getenv("DEPLOYMENT")
 
+# openai.api_type = "azure"
+# openai.api_key = open_ai_key
+# openai.api_base = open_ai_api_endpoints
+# openai.api_version = "2023-05-15"
+
 client = AzureOpenAI(api_key=open_ai_key,api_version="2024-10-21",azure_endpoint=open_ai_api_endpoints)
+
 
 
 # def generate_embeddings(text):
@@ -24,24 +31,66 @@ client = AzureOpenAI(api_key=open_ai_key,api_version="2024-10-21",azure_endpoint
 #     # )
 #     return "response['data'][0]['embedding']"
 
-# def call_gpt_api(json_output):
-#     system_prompt = build_system_prompt()
-#     user_prompt = build_user_prompt(json_output)
-#     messages = [system_prompt, user_prompt]
+def get_agent_transcript(agentId):
+    #return agentId
+    transcripts_df = pd.read_csv(f"data/{agentId}.csv")
+    transcripts_df.columns = transcripts_df.columns.str.strip()
 
-#     completion = client.chat.completions.create( model=deployment, messages=messages,temperature=0.3,max_tokens=100 )
-    
-#     content = completion.choices[0].message.content
-#     return content
+    transcripts_df['speaker'] = transcripts_df['speaker'].astype(str)
+    transcripts_df['time'] = transcripts_df['time'].astype(str)
+    transcripts_df['text'] = transcripts_df['text'].astype(str)
 
+    formatted_data = transcripts_df.to_dict(orient='records')
+
+    json_output = json.dumps(formatted_data, indent=2).replace("\n", "\\r\\n")
+
+    return json_output
+
+def call_gpt_api(json_output):
+    system_prompt = build_system_prompt()
+    user_prompt = build_user_prompt(json_output)
+    messages = [system_prompt, user_prompt]
+
+    completion = client.chat.completions.create( model=deployment, messages=messages,temperature=0.3,max_tokens=500 )
     
+    content = completion.choices[0].message.content
+    return content
+
+# if __name__ == "__main__":
+#     query_text = "Provide feedback for the agent's sales pitch throughout the day."
+#     #res = call_gpt_api(json_output)
+#     #print(get_agent_transcript("PW47633"))
 
 app = Flask(__name__)
 
+@app.route('/getagenttranscript', methods=['GET'])
+def getagenttranscript():
+    agentId = request.args.get('agentId')
+
+    if not agentId:
+        return jsonify({"data": None, "isSuccess": False, "message": "agentId is required"}), 400
+    transcript = get_agent_transcript(agentId)
+    return jsonify({"data": transcript,"isSuccess":True})
+
+
+@app.route('/analysescripts', methods=['GET'])
+def analysescripts():
+    agent_ids_str = os.getenv("AGENT_IDS")
+
+    read_transcripts =[]
+
+    agents = ast.literal_eval(agent_ids_str)
+    
+    for agent in agents:
+        transcript_agent = get_agent_transcript(agent)
+        review = call_gpt_api(transcript_agent)
+        read_transcripts.append({ f"{agent}": transcript_agent, "review" : review})
+
+    return jsonify({"agents":agents,"data": read_transcripts, "isSuccess": False, "message": "agentId is required"}), 400
 
 @app.route('/', methods=['GET'])
 def server_running():
-    return jsonify({"message": "Welcome","isSuccess":True}), 200
+    return jsonify({"message": "Welcome","isSuccess":True})
 
 @app.route('/submit', methods=['POST'])
 def submit_data():
@@ -59,8 +108,6 @@ def submit_data():
         "isSuccess": True
     }
     return jsonify(response), 200
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils/gpt_prompt.py
+++ utils/gpt_prompt.py
@@ -1,7 +1,7 @@
 def build_system_prompt():
     return {
         "role": "system",
-        "content": "You are an AI auditor that analyzes sales agent calls to detect repetition, interruptions, and pitch issues..."
+        "content": "Analyze the conversation transcript to evaluate the agent's performance. Provide feedback on their communication skills, identify any behavioral issues such as lack of empathy or patience, assess the overall sentiment of the interaction, and note any instances of unnecessary repetitions. Your analysis should include a 150-word description that covers the agent's communication effectiveness, behavioral professionalism, emotional dynamics, and redundancy in information delivery. Provide data in points like 2 point for each problem and dont just include negatives includes positives also as you are an audit bot"
     }
 
 def build_user_prompt(transcripts):
